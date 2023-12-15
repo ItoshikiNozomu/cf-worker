5cb35711b055684397d161704c98e3b308b1f5a5
lh <luhuan0827@outlook.com>
2023-12-15 17:56:51 +0800

    update files

---
+++ apps/cf/dist/index.js
@@ -0,0 +1,37 @@
+// import welcome from "welcome.html";
+
+/**
+ * @typedef {Object} Env
+ */
+
+export default {
+	/**
+	 * @param {Request} request
+	 * @param {Env} env
+	 * @param {ExecutionContext} ctx
+	 * @returns {Promise<Response>}
+	 */
+	async fetch(request, env, ctx) {
+		const url = new URL(request.url);
+		console.log(`Hello ${navigator.userAgent} at path ${url.pathname}!`);
+
+		if (url.pathname === "/api") {
+			// You could also call a third party API here
+			const data = await import("./data.js");
+			return Response.json(data);
+		}else if(url.pathname === '/mono-clash-config'){
+			const data = await fetch('https://connect.applecross.link/clash/861692/CJ1FLF2u9nzp',{
+				headers:{
+					'user-agent':'shadowrocket/4.4'
+				}
+			})
+			// console.log(await data.text())
+			return data
+		}
+		return new Response("welcome", {
+			headers: {
+				"content-type": "text/html",
+			},
+		});
+	},
+};
